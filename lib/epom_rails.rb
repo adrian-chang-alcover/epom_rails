8005d0023fd8bd12e88a7ca16673077c02b49c66
Adrian Chang <adrian.chang.alcover@gmail.com>
2015-06-04 09:23:17 -0400

    private   def self.define_before_save(klass, fields)   	before_save do   		self.name='saved in Epom'   	end   end

--- lib/epom_rails.rb
+++ lib/epom_rails.rb
@@ -3,7 +3,6 @@ require 'epom_rails/acts_as'
 
 module EpomRails
 	def method_missing(method_name, *args)
-		puts method_name
 		super
 	end
 end
--- lib/epom_rails/acts_as.rb
+++ lib/epom_rails/acts_as.rb
@@ -3,6 +3,11 @@ class ActiveRecord::Base
   def self.acts_as(klass, fields = {})
     include EpomRails
 
+    define_before_save(klass, fields)
+  end
+
+  private
+  def self.define_before_save(klass, fields)
   	before_save do 
   		self.name='saved in Epom' 
   	end
