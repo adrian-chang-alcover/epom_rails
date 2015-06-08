f906386e788a8a1f0036e74468489f2078a2d645
Adrian Chang <adrian.chang.alcover@gmail.com>
2015-06-08 15:58:05 -0400

    rails generate migration AddEpomIdToCampaigns epom_id:integer rake db:migrate

--- test/dummy/app/models/campaign.rb
+++ test/dummy/app/models/campaign.rb
@@ -1,2 +1,3 @@
 class Campaign < ActiveRecord::Base
+	acts_as_campaign epom_id: 'id', name: 'name', description: 'description', advertiser_id: 'advertiser_id', weight: 'weight'
 end
--- test/dummy/db/schema.rb
+++ test/dummy/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150608194916) do
+ActiveRecord::Schema.define(version: 20150608195747) do
 
   create_table "advertisers", force: :cascade do |t|
     t.string   "name"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 20150608194916) do
     t.integer  "weight"
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
+    t.integer  "epom_id"
   end
 
 end
