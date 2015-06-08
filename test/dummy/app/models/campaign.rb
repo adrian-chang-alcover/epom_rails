dd2796a885704def43f2f8517c2a39dd9e5edaa2
Adrian Chang <adrian.chang.alcover@gmail.com>
2015-06-08 16:04:34 -0400

    19 runs, 48 assertions, 0 failures, 0 errors, 0 skips

--- test/dummy/app/models/campaign.rb
+++ test/dummy/app/models/campaign.rb
@@ -1,3 +1,3 @@
 class Campaign < ActiveRecord::Base
-	acts_as_campaign epom_id: 'id', name: 'name', description: 'description', advertiser_id: 'advertiser_id', weight: 'weight'
+	acts_as_campaign epom_id: 'id', name: 'name', description: 'description', advertiser_id: 'advertiserId', weight: 'weight'
 end
