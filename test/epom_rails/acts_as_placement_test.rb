328caa00bd73629aa60f24265365db32f786dd74
Adrian Chang <adrian.chang.alcover@gmail.com>
2015-06-22 14:48:43 -0400

    Finished in 10.952160s, 0.3652 runs/s, 1.2783 assertions/s.
    4 runs, 14 assertions, 0 failures, 0 errors, 0 skips

--- test/epom_rails/acts_as_placement_test.rb
+++ test/epom_rails/acts_as_placement_test.rb
@@ -29,23 +29,23 @@ class ActsAsPlacementTest < ActiveSupport::TestCase
 	  	assert_equal placement.send(epom_field('name')), epom_placement['name']
 	end
 
-	# test 'delete an placement' do
-	# 	placement = placements(:one)	  	
+	test 'delete an placement' do
+		placement = placements(:one)	  	
 
-	#   	assert	placement.save
-	#   	assert_instance_of Fixnum, placement.send(epom_field('id'))
+	  	assert	placement.save
+	  	assert_instance_of Fixnum, placement.send(epom_field('id'))
 
-	#   	epom_id = placement.send(epom_field('id'))
-	#   	assert placement.destroy
-	# end
+	  	epom_id = placement.send(epom_field('id'))
+	  	assert placement.destroy
+	end
 
-	# test 'epom methods' do
-	# 	placement = placements(:one)	  	
+	test 'epom methods' do
+		placement = placements(:one)	  	
 
-	#   	assert	placement.save
-	#   	assert_instance_of Fixnum, placement.send(epom_field('id'))
+	  	assert	placement.save
+	  	assert_instance_of Fixnum, placement.send(epom_field('id'))
 
-	# 	response = Placement.get_placements_zones({:placementId => placement.send(epom_field('id'))}, {})
-	# 	assert_instance_of Array, response	
-	# end
+		epom_response = Epom::Placement.get_placement_summary({}, {:placementIds => placement.send(epom_field('id'))})
+	  	assert_instance_of Array, epom_response
+	end
 end
