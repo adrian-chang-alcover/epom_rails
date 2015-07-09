73d2d8c7d9aaf1ce9b76cdc225572f1aaf9d35f7
Adrian Chang <adrian.chang.alcover@gmail.com>
2015-07-09 08:29:28 -0400

    Finished in 77.436700s, 0.8652 runs/s, 2.0791 assertions/s. 67 runs, 161 assertions, 0 failures, 0 errors, 0 skips

--- lib/epom_rails.rb
+++ lib/epom_rails.rb
@@ -4,8 +4,6 @@ require 'epom_rails/acts_as'
 module EpomRails
 	attr_accessor :epom_klass, :epom_fields
 
-	Epom::EpomElement.login(ENV['username'], ENV['password'])
-
 	def method_missing(method_name, *args)
 		if epom_klass.respond_to?(method_name)
 			epom_klass.send(method_name, args[0], args[1])
