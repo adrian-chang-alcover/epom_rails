c435ea563d7d2c356fba7b2fb6f4b4ce30cd1d2b
Adrian Chang <adrian.chang.alcover@gmail.com>
2015-07-20 18:29:52 -0400

    def self.has_many_wrapper(options)   	association = has_many options[:name].to_sym, options.except(:name)
      	self.class_eval do
      		alias_method "#{options[:name]_original}", options[:name]
      	end
      end

--- lib/epom_rails/acts_as_advertiser.rb
+++ lib/epom_rails/acts_as_advertiser.rb
@@ -1,11 +1,18 @@
 class ActiveRecord::Base 
 
-  def self.acts_as_advertiser(fields = {})
+  def self.acts_as_advertiser(parameters)
+  	fields = parameters.select{|key, value| self.column_names.include? key}
     acts_as(Epom::Advertiser, fields)
+
+    parameters[:has_many].each {|option| has_many_wrapper(option)} if parameters[:has_many]
   end
 
-  def campaignss
-  	byebug
-  end
+  private
+  def self.has_many_wrapper(options)
+  	association = has_many options[:name].to_sym, options.except(:name)
 
+  	self.class_eval do 
+  		alias_method "#{options[:name]_original}", options[:name] 
+  	end
+  end
 end
--- test/dummy/app/models/advertiser.rb
+++ test/dummy/app/models/advertiser.rb
@@ -1,3 +1,3 @@
 class Advertiser < ActiveRecord::Base
-	acts_as_advertiser epom_id: 'id', name: 'name', contact_name: 'contactName', contact_email: 'contactEmail', description: 'description'
+	acts_as_advertiser epom_id: 'id', name: 'name', contact_name: 'contactName', contact_email: 'contactEmail', description: 'description', has_many: [{:name => 'campaigns', :class_name => 'Campaign'}]
 end
