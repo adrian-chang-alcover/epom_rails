01a7e4e64cd5d84b756ea22b5ebbc86b2fdaa38c
Adrian Chang <adrian.chang.alcover@gmail.com>
2015-07-31 11:26:49 -0400

    rails generate scaffold Publisher name:string url:string description:text email:string category_id:integer rake db:migrate

--- test/dummy/db/schema.rb
+++ test/dummy/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150622181754) do
+ActiveRecord::Schema.define(version: 20150731152459) do
 
   create_table "advertisers", force: :cascade do |t|
     t.string   "name"
@@ -59,6 +59,16 @@ ActiveRecord::Schema.define(version: 20150622181754) do
     t.integer  "size_width"
     t.datetime "created_at",     null: false
     t.datetime "updated_at",     null: false
+  end
+
+  create_table "publishers", force: :cascade do |t|
+    t.string   "name"
+    t.string   "url"
+    t.text     "description"
+    t.string   "email"
+    t.integer  "category_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
   create_table "sites", force: :cascade do |t|
---
+++ test/dummy/test/controllers/publishers_controller_test.rb
@@ -0,0 +1,49 @@
+require 'test_helper'
+
+class PublishersControllerTest < ActionController::TestCase
+  setup do
+    @publisher = publishers(:one)
+  end
+
+  test "should get index" do
+    get :index
+    assert_response :success
+    assert_not_nil assigns(:publishers)
+  end
+
+  test "should get new" do
+    get :new
+    assert_response :success
+  end
+
+  test "should create publisher" do
+    assert_difference('Publisher.count') do
+      post :create, publisher: { category_id: @publisher.category_id, description: @publisher.description, email: @publisher.email, name: @publisher.name, url: @publisher.url }
+    end
+
+    assert_redirected_to publisher_path(assigns(:publisher))
+  end
+
+  test "should show publisher" do
+    get :show, id: @publisher
+    assert_response :success
+  end
+
+  test "should get edit" do
+    get :edit, id: @publisher
+    assert_response :success
+  end
+
+  test "should update publisher" do
+    patch :update, id: @publisher, publisher: { category_id: @publisher.category_id, description: @publisher.description, email: @publisher.email, name: @publisher.name, url: @publisher.url }
+    assert_redirected_to publisher_path(assigns(:publisher))
+  end
+
+  test "should destroy publisher" do
+    assert_difference('Publisher.count', -1) do
+      delete :destroy, id: @publisher
+    end
+
+    assert_redirected_to publishers_path
+  end
+end
