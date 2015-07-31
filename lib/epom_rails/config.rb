fdf1f017948c4e67536fdd63cbf6f55a902ddf8c
Adrian Chang <adrian.chang.alcover@gmail.com>
2015-07-30 22:19:53 -0400

    EpomRails.configure do |config|     config.offline = false
        config.advertiser = {}
        config.advertiser[:fields] = {epom_id: 'id', name: 'name', contact_name: 'contactName', contact_email: 'contactEmail', description: 'description'}
        config.advertiser[:has_many] = [{:name => 'campaigns', :epom_element => 'Campaign', :model => 'Campaign'}]
    
        config.campaign = {}
        config.campaign[:fields] = {epom_id: 'id', name: 'name', description: 'description', advertiser_id: 'advertiserId', weight: 'weight'}
        config.campaign[:has_many] = [{:name => 'banners', :epom_element => 'Banner', :model => 'Banner'}]
    
        config.banner = {}
        config.banner[:fields] = {epom_id: 'id', placement_type: 'placementType', campaign_id: 'campaignId', weight: 'weight', image_banner_link: 'imageBannerLink', url: 'url', name: 'name', banner_type: 'bannerType', ad_unit_id: 'adUnitId', ad_unit_width: 'adUnitWidth', ad_unit_height: 'adUnitHeight'}
    
        config.site = {}
        config.site[:fields] = {epom_id: 'id', name: 'name', url: 'url', description: 'description', email: 'email', category_id: 'categoryId'}
        config.site[:has_many] = [{:name => 'zones', :epom_element => 'Zone', :model => 'Zone'}]
    
        config.zone = {}
        config.zone[:fields] = {epom_id: 'id', name: 'name', description: 'description', site_id: 'siteId'}
        config.zone[:has_many] = [{:name => 'placements', :epom_element => 'Placement', :model => 'Placement'}]
    
        config.placement = {}
        config.placement[:fields] = {epom_id: 'id', zone_id: 'zoneId', placement_type: 'type', name: 'name', ad_unit_id: 'adUnitId', size_height: 'size.height', size_width: 'size.width'}
      end

--- lib/epom_rails/config.rb
+++ lib/epom_rails/config.rb
@@ -32,11 +32,27 @@ module EpomRails
   # this is ugly. why can't we pass the default value to config_accessor...?
   EpomRails.configure do |config|
     config.offline = false
-    config.advertiser = {fields: {epom_id: 'id', name: 'name', contact_name: 'contactName', contact_email: 'contactEmail', description: 'description'}, has_many: [{:name => 'campaigns', :epom_element: 'Campaign', :model => 'Campaign'}]}
-    config.campaign = {fields: {epom_id: 'id', name: 'name', description: 'description', advertiser_id: 'advertiserId', weight: 'weight'}, has_many: [{:name => 'banners', :epom_element: 'Banner', :model => 'Banner'}]}
-    config.banner = {fields: {epom_id: 'id', placement_type: 'placementType', campaign_id: 'campaignId', weight: 'weight', image_banner_link: 'imageBannerLink', url: 'url', name: 'name', banner_type: 'bannerType', ad_unit_id: 'adUnitId', ad_unit_width: 'adUnitWidth', ad_unit_height: 'adUnitHeight'}}
-    config.site = {fields: {epom_id: 'id', name: 'name', url: 'url', description: 'description', email: 'email', category_id: 'categoryId'}, has_many: [{:name => 'zones', :epom_element: 'Zone', :model => 'Zone'}]}
-    config.zone = {fields: {epom_id: 'id', name: 'name', description: 'description', site_id: 'siteId'}, has_many: [{:name => 'placements', :epom_element: 'Placement', :model => 'Placement'}]}
-    config.placement = {fields: {epom_id: 'id', zone_id: 'zoneId', placement_type: 'type', name: 'name', ad_unit_id: 'adUnitId', size_height: 'size.height', size_width: 'size.width'}}
+
+    config.advertiser = {}
+    config.advertiser[:fields] = {epom_id: 'id', name: 'name', contact_name: 'contactName', contact_email: 'contactEmail', description: 'description'}
+    config.advertiser[:has_many] = [{:name => 'campaigns', :epom_element => 'Campaign', :model => 'Campaign'}]
+
+    config.campaign = {}
+    config.campaign[:fields] = {epom_id: 'id', name: 'name', description: 'description', advertiser_id: 'advertiserId', weight: 'weight'}
+    config.campaign[:has_many] = [{:name => 'banners', :epom_element => 'Banner', :model => 'Banner'}]
+
+    config.banner = {}
+    config.banner[:fields] = {epom_id: 'id', placement_type: 'placementType', campaign_id: 'campaignId', weight: 'weight', image_banner_link: 'imageBannerLink', url: 'url', name: 'name', banner_type: 'bannerType', ad_unit_id: 'adUnitId', ad_unit_width: 'adUnitWidth', ad_unit_height: 'adUnitHeight'}
+
+    config.site = {}
+    config.site[:fields] = {epom_id: 'id', name: 'name', url: 'url', description: 'description', email: 'email', category_id: 'categoryId'}
+    config.site[:has_many] = [{:name => 'zones', :epom_element => 'Zone', :model => 'Zone'}]
+
+    config.zone = {}
+    config.zone[:fields] = {epom_id: 'id', name: 'name', description: 'description', site_id: 'siteId'}
+    config.zone[:has_many] = [{:name => 'placements', :epom_element => 'Placement', :model => 'Placement'}]
+
+    config.placement = {}
+    config.placement[:fields] = {epom_id: 'id', zone_id: 'zoneId', placement_type: 'type', name: 'name', ad_unit_id: 'adUnitId', size_height: 'size.height', size_width: 'size.width'}
   end
 end
