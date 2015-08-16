2f4b68dfd69b714fd6cd8aa4a0afc890d37f60b9
Adrian Chang <adrian.chang.alcover@gmail.com>
2015-08-16 15:08:43 -0400

    if campaigns_association     	Advertiser.class_eval do     		def self.before_add_a_campaign(callback, advertiser, campaign) 					puts campaign 				end     	end
    			before_add_for_campaigns << proc {|callback, advertiser, campaign| self.before_add_a_campaign(callback, advertiser, campaign);}
        end

--- lib/epom_rails/acts_as_advertiser.rb
+++ lib/epom_rails/acts_as_advertiser.rb
@@ -8,10 +8,16 @@ class ActiveRecord::Base
   end
 
   def self.override_has_many_campaigns
-  	campaigns_association = EpomRails.config.campaign[:has_many].find{|a| a[:epom_element]=="Campaign"}
+  	campaigns_association = EpomRails.config.advertiser[:has_many].find{|a| a[:epom_element]=="Campaign"}
   	
     if campaigns_association
+    	Advertiser.class_eval do
+    		def self.before_add_a_campaign(callback, advertiser, campaign)
+					puts campaign
+				end
+    	end
       
+			before_add_for_campaigns << proc {|callback, advertiser, campaign| self.before_add_a_campaign(callback, advertiser, campaign);}
     end  
   end
 
--- test/dummy/app/models/advertiser.rb
+++ test/dummy/app/models/advertiser.rb
@@ -1,11 +1,4 @@
 class Advertiser < ActiveRecord::Base
 	has_many :campaigns
 	acts_as_advertiser
-
-	def self.before_add_a_campaign(callback, advertiser, campaign)
-		puts [callback, advertiser, campaign]
-	end
-
-	before_add_for_campaigns << proc {|callback, advertiser, campaign| before_add_a_campaign(callback, advertiser, campaign)}
-
 end
