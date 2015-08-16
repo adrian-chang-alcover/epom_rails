a1e41a0e448f6c97b5e0a6a8f14fa2449b51f6ff
Adrian Chang <adrian.chang.alcover@gmail.com>
2015-08-16 14:32:01 -0400

    def self.before_add_a_campaign(callback, advertiser, campaign) 	puts [callback, advertiser, campaign] end
    before_add_for_campaigns << proc {|callback, advertiser, campaign| before_add_a_campaign(callback, advertiser, campaign)}

--- lib/epom_rails/acts_as_advertiser.rb
+++ lib/epom_rails/acts_as_advertiser.rb
@@ -3,6 +3,16 @@ class ActiveRecord::Base
   def self.acts_as_advertiser(params = {})
     EpomRails.config.advertiser[:model] = self
     acts_as(Epom::Advertiser, params)
+
+    override_has_many_campaigns
+  end
+
+  def self.override_has_many_campaigns
+  	campaigns_association = EpomRails.config.campaign[:has_many].find{|a| a[:epom_element]=="Campaign"}
+  	
+    if campaigns_association
+      
+    end  
   end
 
 end
