2b0a3425b0336f9cbfec23ba85d96fef9cf858db
Adrian Chang <adrian.chang.alcover@gmail.com>
2015-08-17 12:44:03 -0400

    attr_accessor :epom_advertiser_id
    	def epom_advertiser_id
    		advertiser.epom_id if advertiser
    	end

--- test/dummy/app/models/campaign.rb
+++ test/dummy/app/models/campaign.rb
@@ -2,4 +2,10 @@ class Campaign < ActiveRecord::Base
 	belongs_to :advertiser
 	has_many :banners
 	acts_as_campaign fields: {epom_id: 'id', name: 'name', description: 'description', weight: 'weight', epom_advertiser_id: 'advertiserId'}
+
+	attr_accessor :epom_advertiser_id
+
+	def epom_advertiser_id
+		advertiser.epom_id if advertiser
+	end
 end
