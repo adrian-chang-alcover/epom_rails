8d4c78e8c8cd585f245d2cb574f86d29729fcec7
Adrian Chang <adrian.chang.alcover@gmail.com>
2015-08-19 18:07:21 -0400

    'advertiser.epom_id' => 'advertiserId' 'campaign.epom_id' => 'campaignId' 'site.epom_id' => 'siteId' 'zone.epom_id' => 'zoneId'

--- lib/generators/epom_rails/config/templates/epom_rails.rb
+++ lib/generators/epom_rails/config/templates/epom_rails.rb
@@ -6,12 +6,12 @@ EpomRails.configure do |config|
   config.advertiser[:has_many] = [{:name => 'campaigns', :epom_element => 'Campaign', :model => 'Campaign'}]
 
   config.campaign = {}
-  config.campaign[:fields] = {epom_id: 'id', name: 'name', description: 'description', weight: 'weight'}
+  config.campaign[:fields] = {epom_id: 'id', name: 'name', description: 'description', weight: 'weight', 'advertiser.epom_id' => 'advertiserId'}
   config.campaign[:has_many] = [{:name => 'banners', :epom_element => 'Banner', :model => 'Banner'}]
   config.campaign[:belongs_to] = [{:name => 'advertiser', :epom_element => 'Advertiser', :model => 'Advertiser'}]
 
   config.banner = {}
-  config.banner[:fields] = {epom_id: 'id', placement_type: 'placementType', campaign_id: 'campaignId', weight: 'weight', image_banner_link: 'imageBannerLink', url: 'url', name: 'name', banner_type: 'bannerType', ad_unit_id: 'adUnitId', ad_unit_width: 'adUnitWidth', ad_unit_height: 'adUnitHeight'}
+  config.banner[:fields] = {epom_id: 'id', placement_type: 'placementType', 'campaign.epom_id' => 'campaignId', weight: 'weight', image_banner_link: 'imageBannerLink', url: 'url', name: 'name', banner_type: 'bannerType', ad_unit_id: 'adUnitId', ad_unit_width: 'adUnitWidth', ad_unit_height: 'adUnitHeight'}
   config.banner[:has_many] = [{:name => 'placements', :epom_element => 'Placement', :model => 'Placement'}]
   config.banner[:belongs_to] = [{:name => 'campaign', :epom_element => 'Campaign', :model => 'Campaign'}]
 
@@ -20,12 +20,12 @@ EpomRails.configure do |config|
   config.site[:has_many] = [{:name => 'zones', :epom_element => 'Zone', :model => 'Zone'}]
 
   config.zone = {}
-  config.zone[:fields] = {epom_id: 'id', name: 'name', description: 'description', site_id: 'siteId'}
+  config.zone[:fields] = {epom_id: 'id', name: 'name', description: 'description', 'site.epom_id' => 'siteId'}
   config.zone[:has_many] = [{:name => 'placements', :epom_element => 'Placement', :model => 'Placement'}]
   config.zone[:belongs_to] = [{:name => 'site', :epom_element => 'Site', :model => 'Site'}]
 
   config.placement = {}
-  config.placement[:fields] = {epom_id: 'id', zone_id: 'zoneId', placement_type: 'type', name: 'name', ad_unit_id: 'adUnitId', size_height: 'size.height', size_width: 'size.width'}
+  config.placement[:fields] = {epom_id: 'id', 'zone.epom_id' => 'zoneId', placement_type: 'type', name: 'name', ad_unit_id: 'adUnitId', size_height: 'size.height', size_width: 'size.width'}
   config.placement[:has_many] = [{:name => 'banners', :epom_element => 'Banner', :model => 'Banner'}]
   config.placement[:belongs_to] = [{:name => 'zone', :epom_element => 'Zone', :model => 'Zone'}]
 end
