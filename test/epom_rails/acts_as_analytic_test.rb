eb1cad5d7a5294221d13e755fbe9725dbba1f804
Adrian Chang <adrian.chang.alcover@gmail.com>
2015-08-26 21:45:35 -0400

    test 'analytic_from_epom' 1 runs, 2 assertions, 0 failures, 0 errors, 0 skips

--- test/epom_rails/acts_as_analytic_test.rb
+++ test/epom_rails/acts_as_analytic_test.rb
@@ -2,7 +2,8 @@ require 'test_helper'
 
 class ActsAsAnalyticTest < ActiveSupport::TestCase
 	test 'analytic_from_epom' do
-		Analytic.analytic_from_epom
-		byebug
+		analytics = Analytic.analytic_from_epom({range: 'CURRENT_MONTH'})
+		assert_instance_of Array, analytics
+		assert_instance_of Analytic, analytics[0] if analytics.count > 0
 	end
 end
